409574f9a85958aa5f75d6fbd2ddd581bdad51ee
unknown <cezaryulin@gmail.com>
2025-10-17 16:37:45 +0300

    add server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const Koa = require('koa');
+const path = require('path');
+const serve = require('koa-static');
+const Router = require('koa-router')
+const staticDirPath = path.join(__dirname, '');
+const nodeModulesDirPath = path.join(__dirname, 'node_modules');
+
+const server = new Koa();
+
+const router = new Router();
+
+router.post('/upload/:filename', async ctx => {});
+
+router.get('/result/:filename', async ctx => {});
+
+server.use(router.routes()).use(router.allowedMethods());
+
+server.use(serve(staticDirPath));
+server.use(serve(nodeModulesDirPath));
+
+const PORT = 3000;
+server.listen(PORT, () => console.log(`Server Listening on PORT ${PORT} ..`));
