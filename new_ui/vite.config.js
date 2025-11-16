54ed5887cd7a51181a1d68fc1969864072f07054
unknown <cezaryulin@gmail.com>
2025-11-16 13:39:21 +0300

    add proxy for vite issue #15

--- new_ui/vite.config.js
+++ new_ui/vite.config.js
@@ -3,4 +3,22 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 5173,  // Vue на порту 5173
+    proxy: {
+      '/upload': {
+        target: 'http://localhost:3000',  // Бэкенд на 3000
+        changeOrigin: true,
+      },
+      '/uploads': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
+      '/socket.io': {
+        target: 'http://localhost:3000',
+        ws: true,
+        changeOrigin: true,
+      }
+    }
+  }
 })
