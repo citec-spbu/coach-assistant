0f28d764c77587d97c7d2af123d5d950b469bc61
ErmolenkoSV <87709507+ErmolenkoSV@users.noreply.github.com>
2025-11-23 20:50:24 +0500

    Add files via upload

--- dance_classifier/__init__.py
+++ dance_classifier/__init__.py
@@ -1,57 +1,15 @@
-"""
-Классификатор танцевальных фигур
+"""Модули для подготовки данных"""
 
-Система классификации танцевальных фигур на основе поз,
-извлеченных из видео с помощью модуля DancePose.
-"""
-
-__version__ = '1.0.0'
-__author__ = 'Dance Classifier Team'
-
-# Основные модули
-from .data_preparation import extract_poses, feature_extraction, dataset_builder
-from .models import gru_classifier, tcn_classifier
-from .training import train
-from .inference import predict
-from .utils import helpers
-
-__all__ = [
-    'extract_poses',
-    'feature_extraction',
-    'dataset_builder',
-    'gru_classifier',
-    'tcn_classifier',
-    'train',
-    'predict',
-    'helpers'
-]
+# Ленивый импорт для избежания циклических зависимостей
+# Используйте прямые импорты: from dance_classifier.data_preparation import dataset_builder
 
 
-Классификатор танцевальных фигур
 
-Система классификации танцевальных фигур на основе поз,
-извлеченных из видео с помощью модуля DancePose.
-"""
 
-__version__ = '1.0.0'
-__author__ = 'Dance Classifier Team'
 
-# Основные модули
-from .data_preparation import extract_poses, feature_extraction, dataset_builder
-from .models import gru_classifier, tcn_classifier
-from .training import train
-from .inference import predict
-from .utils import helpers
 
-__all__ = [
-    'extract_poses',
-    'feature_extraction',
-    'dataset_builder',
-    'gru_classifier',
-    'tcn_classifier',
-    'train',
-    'predict',
-    'helpers'
-]
+
+
+
 
 
